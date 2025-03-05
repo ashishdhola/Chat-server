f58e266fd7db5f796498b4a5fa60e9a87b7b33a6
Ashish <ashishdhola0.7@gmail.com>
2025-03-05 14:43:20 +0530

    backend added

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import dotenv from "dotenv";
+import express from "express";
+
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const host = process.env.HOST;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://172.19.3.48:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, host, () => {
+  console.log("server is running on PORT:" + PORT + " HOST:" + host);
+  connectDB();
+});
