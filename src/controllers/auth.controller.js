c1e5d6771a2918d194175d314d072b6e1a19551a
Ashish <ashishdhola0.7@gmail.com>
2025-03-05 16:33:28 +0530

    body data

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -52,6 +52,7 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
+  console.log(req.body);
   try {
     const user = await User.findOne({ email });
 
