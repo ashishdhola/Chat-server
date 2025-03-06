e2e988b41f6fc224d28182362b0a0d1ed6399ca5
Ashish <ashishdhola0.7@gmail.com>
2025-03-06 15:08:06 +0530

    edded

--- src/index.js
+++ src/index.js
@@ -24,21 +24,23 @@ app.use(cookieParser());
 //     credentials: true,
 //   })
 // );
-
 const allowedOrigins = [
-  "https://chat-react-ashish-dholas-projects.vercel.app",
-  "https://chat-react-rho-flax.vercel.app"
+  "https://chat-react-rho-flax.vercel.app",
+  "https://chat-react-ashish-dholas-projects.vercel.app"
 ];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
